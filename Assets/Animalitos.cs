ea09fa9c3cfd44836f749c85a0f43935e8789d8e
ValentinaHsu <92767924+ValentinaHsu@users.noreply.github.com>
2022-04-11 11:29:52 -0300

    Tema 3

---
+++ Assets/Animalitos.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animalitos : MonoBehaviour
+{
+    public string codigo;
+    public int dias;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int alimento;
+        int precio;
+        if (dias >= 3)
+        {
+            if (codigo == "G")
+            {
+                alimento = 300 * dias;
+                precio = alimento / 100 * 80;
+                Debug.Log("Para ese período se necesitan " + alimento + " gramos de alimento.");
+                Debug.Log("El costo sera de $" + precio);
+
+            }
+            else if (codigo == "PP")
+            {
+                alimento = 400 * dias;
+                precio = alimento / 100 * 80;
+                Debug.Log("Para ese período se necesitan " + alimento + " gramos de alimento.");
+                Debug.Log("El costo sera de $" + precio);
+            }
+            else if (codigo == "PG")
+            {
+                alimento = 700 * dias;
+                precio = alimento / 100 * 80;
+                Debug.Log("Para ese período se necesitan " + alimento + " gramos de alimento.");
+                Debug.Log("El costo sera de $" + precio);
+            }
+            else
+            {
+                Debug.Log("El codigo ingresado no es valido");
+            }
+        }
+        else
+        {
+            Debug.Log("La cantidad de dias no puede ser menor de 3");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
